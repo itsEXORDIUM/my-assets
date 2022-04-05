f085f2aa861c550975908d631f966961f755da2c
Caroline Chambre <caroline.chambre@orange.fr>
2022-04-05 18:39:19 +0200

    bill skarsgard

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const { src, dest, watch, series } = require('gulp');
 // const debug = require('gulp-debug');
 
 function toJson() {
-    return src('./yaml/testexofinds_fcs.yaml')
+    return src('./yaml/exofinds_fcs.yaml')
         .pipe(yaml({ safe: true }))
         .pipe(dest('./jsons/'))
 }
 
 function watchTask() {
-    watch('yaml/testexofinds_fcs.yaml', toJson)
+    watch('yaml/exofinds_fcs.yaml', toJson)
     // watch('src/*.js', series(clean, javascript)),
 }
 
