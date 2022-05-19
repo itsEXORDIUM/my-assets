a6d034702cb89cc1a2ba563f5b40f84383952d12
Caroline Chambre <caroline.chambre@orange.fr>
2022-05-19 17:59:45 +0200

    19/05 part 1

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const { src, dest, watch, series } = require('gulp');
 // const debug = require('gulp-debug');
 
 function toJson() {
-    return src('./yaml/exofinds_fcs_new.yaml')
+    return src('./yaml/exofinds_fcs.yaml')
         .pipe(yaml({ safe: true }))
         .pipe(dest('./jsons/'))
 }
 
 function watchTask() {
-    watch('yaml/exofinds_fcs_new.yaml', toJson)
+    watch('yaml/exofinds_fcs.yaml', toJson)
     // watch('src/*.js', series(clean, javascript)),
 }
 
